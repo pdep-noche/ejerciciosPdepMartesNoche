87fe4dc501f20aea6b3fd05e4cd5dc9f2c3f98e8
vpennella <vpennella@gmail.com>
2020-04-07 21:28:27 -0300

    nuevos cambios

--- ejercicio.hs
+++ ejercicio.hs
@@ -1,10 +1,14 @@
 
 
+doble :: Integer -> Integer
 doble nro = nro * 2
+
 signo :: Integer -> Integer 
 signo nro | nro > 0 = 1
           | nro < 0 = -1
           | otherwise = 0
+
+f nro = (doble.signo) nro
 
 maximo :: Integer -> Integer -> Integer
 maximo nro otroNum | nro > otroNum = nro
