dbe195c470b9d06ae2ab6a308860f1ac8094b519
vpennella <vpennella@gmail.com>
2020-04-21 22:03:26 -0300

    ejercicio 2

--- tiposPropiosOrdenSuperior.hs
+++ tiposPropiosOrdenSuperior.hs
@@ -6,3 +6,19 @@ sueldo (Comun _ suel) = suel
 sueldo (Jefe _ suel cantidad) = suel + plus cantidad
 
 plus cantidad = 500 * cantidad
+ {-Ejercicio 2 -}
+
+data Bebida = Cafe {nombreBebida :: String, azucar :: Integer} |
+            Gaseosa {sabor ::String , azucar :: Integer}
+
+esEnergizante :: Bebida -> Bool
+esEnergizante (Cafe "capuchino" _) = True
+esEnergizante (Gaseosa "pomelo" cantidadAzucar) = cantidadAzucar > 10
+esEnergizante _  = False
+
+type Nombre = String
+type Edad = Int
+data Persona = Estudiante {nom :: Nombre, edad:: Edad}
+
+julia :: Persona
+julia = Estudiante "julia" 20
