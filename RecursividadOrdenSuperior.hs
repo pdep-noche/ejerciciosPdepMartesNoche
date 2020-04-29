7a1b2bd3d1f7c182b0eab4932abc8623853efecd
vpennella <vpennella@gmail.com>
2020-04-28 21:40:14 -0300

    se agrega hasta el ejercicio 5

--- RecursividadOrdenSuperior.hs
+++ RecursividadOrdenSuperior.hs
@@ -10,3 +10,13 @@ promedio notas = (sum notas) `div` (length notas)
 
 promediosSinAplazos :: [Notas] -> [Int]
 promediosSinAplazos  = map (promedio.filter (>=6)) 
+
+aprobo :: Persona -> Bool
+aprobo  = all (>=6).notas
+
+aprobaron :: [Persona] -> [Nombre]
+aprobaron alumnos = (map nombre . filter aprobo) alumnos
+
+productos  = zip 
+
+productos'  = zipWith (\nom precio ->(nom, precio)) 
